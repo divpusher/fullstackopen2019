d40368ed8bc050c3465c3084d081cdcc59b4adeb
divpusher <hello@divpusher.com>
2019-10-06 15:04:24 +0200

    exercise 1.8

--- part 1/unicafe/src/index.js
+++ part 1/unicafe/src/index.js
@@ -7,46 +7,42 @@ const Button = ({ handleClick, text }) => (
 )
 
 
+const Statistics = (props) => {
+  const calcAverage = () => (
+      ((props.good * 1) + (props.bad * -1)) / (props.good + props.neutral + props.bad)
+  )
+
+  const calcPositive = () => (
+      (props.good / (props.good + props.neutral + props.bad)) * 100
+  )
+
+  return (
+    <>  
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.good + props.neutral + props.bad}</p>
+      <p>average {calcAverage() || 0}</p>
+      <p>positive {calcPositive() || 0} %</p>
+    </>
+  )
+}
+
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGoodClick = () => {
-    setGood(good + 1)
-  }
-
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const handleBadClick = () => {
-    setBad(bad + 1)
-  }
-
-  const calcAverage = () => (
-      ((good * 1) + (bad * -1)) / (good + neutral + bad)
-  )
-
-  const calcPositive = () => (
-      (good / (good + neutral + bad)) * 100
-  )
-
-
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={handleGoodClick} text="good" />
-      <Button handleClick={handleNeutralClick} text="neutral" />
-      <Button handleClick={handleBadClick} text="bad" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {calcAverage() || 0}</p>
-      <p>positive {calcPositive() || 0} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
