45f48fcb70feb2b75ef1b09ebb8d3481280ea636
divpusher <hello@divpusher.com>
2019-10-06 14:51:26 +0200

    exercise 1.7

--- part 1/unicafe/src/index.js
+++ part 1/unicafe/src/index.js
@@ -13,19 +13,44 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  const calcAverage = () => (
+      ((good * 1) + (bad * -1)) / (good + neutral + bad)
+  )
+
+  const calcPositive = () => (
+      (good / (good + neutral + bad)) * 100
+  )
+
+
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {calcAverage() || 0}</p>
+      <p>positive {calcPositive() || 0} %</p>
     </div>
   )
 }
+
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
