e3fcac098177b087b96f3953c5b4f510a7616f05
divpusher <hello@divpusher.com>
2019-10-24 21:20:03 +0200

    exercise 4.2.3

--- part 4/blog-list/index.js
+++ part 4/blog-list/index.js
@@ -1,36 +1,5 @@
+const app = require('./app')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
-
-
-const mongoUrl = process.env.MONGODB_URI
-
-console.log('connecting to', mongoUrl) 
-
-// deprecation warning fixes
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
-mongoose
-  .connect(mongoUrl)
-  .then(result => {    
-    console.log('connected to MongoDB')  
-  })  
-  .catch((error) => {    
-    console.log('error connecting to MongoDB:', error.message)  
-  })
-
-
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use('/api/blogs', blogsRouter)
 
 
 app.listen(config.PORT, () => {
