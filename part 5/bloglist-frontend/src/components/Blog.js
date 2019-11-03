792f55fc84e25dd47a8bf7548cdefffdcff0b919
divpusher <hello@divpusher.com>
2019-11-03 17:40:25 +0100

    exercise 5.6

--- part 5/bloglist-frontend/src/components/Blog.js
+++ part 5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+
+const Blog = ({ blog }) => {
+  
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  let visibility = { display: visible ? '' : 'none' }
+
+  
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => toggleVisibility()}>
+        {blog.title} {blog.author}
+
+        <div style={visibility}>
+          <div>{blog.likes} likes <button>like</button></div>
+          <div>added by {blog.user.name}</div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
 
 export default Blog
