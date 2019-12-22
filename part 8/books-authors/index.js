9d841560f846b8ca52d9552e044c94f6ea4ecabf
divpusher <hello@divpusher.com>
2019-12-22 16:12:52 +0100

    exercise 8.6

--- part 8/books-authors/index.js
+++ part 8/books-authors/index.js
@@ -1,4 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server')
+const uuid = require('uuid/v1')
+
+
 
 let authors = [
   {
@@ -108,6 +111,16 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
+
 `
 
 const resolvers = {
@@ -140,6 +153,22 @@ const resolvers = {
       return authorBooks.length
     }
   },
+
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find(a => a.name === args.author)){
+        const author = {
+          name: args.author,
+          id: uuid()
+        }
+        authors = authors.concat(author)
+      }
+
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    }
+  }
 }
 
 
